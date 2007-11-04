a3eb8f4c0dc0acd193d3d596f3d52dd6f0ef695a
carregal <carregal>
2007-11-04 23:48:03 +0000

    Shake dispatcher for CGILua

---
+++ src/apps/shake.lua
@@ -0,0 +1,11 @@
+require"shake"
+KEPLER_TESTS = KEPLER_CONF.."/tests"
+
+local name, rest = cgilua.splitfirst(cgilua.vpath)
+
+if name == nil or name == "" then
+    cgilua.script_path = KEPLER_WEB.."/shake.lp"
+    cgilua.vpath = rest
+    return true -- no response needed
+end
+    
