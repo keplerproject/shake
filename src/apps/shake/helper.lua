7e96709a8a9c9d1b2063f6c80135aed70021e827
carregal <carregal>
2007-11-16 16:26:10 +0000

    Adapting to the new CGILua app format. Using resources (CSS, images) in the app directory. New URL format /shake/cmd/module_name where cmd can be "all" or "module"

---
+++ src/apps/shake/helper.lua
@@ -0,0 +1,158 @@
+-- Helper file for the CGILua based Shake runner
+--
+
+-- Lists the available test modules using a <li>..</li> format
+function ListModules(modules)
+	return function()
+		if not next(modules) then
+		    cgilua.put("<li><strong>(No modules)</strong></li>")
+		else
+		    for _, module_name in ipairs(modules) do
+		        cgilua.put([[<li><strong><a href="]]..cgilua.mkurlpath("module/"..module_name)..
+		        	[[">]]..module_name..[[</a></strong></li>]])
+		    end
+		end
+	end
+end
+
+-- Report the Shake results for all the modules as a HTML table
+function ReportModules(run)
+   return function()
+   local results = run.results
+   for cs, suite in ipairs(results.suites) do
+       cgilua.put("<tr>\n")
+       cgilua.put("<th><strong>"..[[<a href="]]..cgilua.mkurlpath("module/"..suite.title)..[[">]]..suite.title..[[</a></strong></th>]].."\n".."</strong></th>\n")
+       if suite.error then
+           cgilua.put([[<td>]]..suite.passed + suite.failed.."</td>\n")
+           cgilua.put([[<td]]..(((suite.failed > 0) and [[ class="failed">]]..suite.failed) or ">").."</td>\n")
+           cgilua.put([[<td class="error">1</td>]].."\n")
+       elseif suite.failed > 0 then
+           cgilua.put([[<td>]]..suite.passed + suite.failed.."</td>\n")
+           cgilua.put([[<td class="failed">]]..suite.failed.."</td>\n")
+           cgilua.put([[<td class="noerror"></td>]].."\n")
+       else
+           cgilua.put([[<td class="passed">]]..suite.passed + suite.failed.."</td>\n")
+           cgilua.put([[<td class="passed"></td>]].."\n")
+           cgilua.put([[<td class="noerror"></td>]].."\n")
+       end
+       cgilua.put("</tr>\n")
+   end
+   cgilua.put("<tfoot>")
+   cgilua.put("<tr>\n")
+   cgilua.put("<th><strong>Totals</strong></th>\n")
+   cgilua.put("<td>"..results.failed + results.passed.."</td>\n")
+   cgilua.put([[<td class="]]..((results.failed > 0) and ([[failed">]]..results.failed) or [[passed">]]).."</td>\n")
+   cgilua.put([[<td class="]]..((results.errors > 0) and ([[error">]]..results.errors) or [[noerror">]]).."</td>\n")
+   cgilua.put("</tr>\n")
+   cgilua.put("</tfoot>")
+   end
+end
+
+-- Report Shake results of single Module run as a table
+function ReportModule(run)
+   return function()
+       local results = run.results
+       for _, suite in ipairs(results.suites) do
+           if suite.error == -1 then
+               cgilua.put("<p><strong>ERROR</strong>:</p>")
+               cgilua.put("<p>"..suite.error.."</p>")
+           else
+               for _, context in ipairs(suite.contexts) do
+                   if next(context.tests) then
+                       if context.output[1] ~= "" or context.comments then
+                           cgilua.put([[<div class="shakecontext">]].."\n")
+                           for _, output in ipairs(context.output) do
+                               if output and output ~= "" then
+                                   cgilua.put([[<div class="shakeoutput">]]..output.."</div>\n")
+                               end
+                           end
+                           if context.comments and context.comments ~= "" then
+                               cgilua.put([[<div class="shakecomment">]]..context.comments.."</div>\n")
+                           end
+                           cgilua.put("</div>")
+                       end
+                       cgilua.put([[
+                       <table class="shake">
+                           <thead>
+                               <tr>
+                                   <th scope="col">Line #</th>
+                                   <th scope="col"></th>
+                                   <th scope="col"><strong>Expected</strong></th>
+                                   <th scope="col"><strong>Actual</strong></th>
+                                   <th scope="col"><strong>Message</strong></th>
+                               </tr>
+                           </thead>]])
+                       for _, test in ipairs (context.tests) do
+                           local linenumber = test.linenumber or "???"
+                           local op = test.op
+                           local val2 = tostring(test.val2)
+                           
+                           if not op then
+                               val2 = "<em><strong>True value</strong></em>"
+                           end
+                           
+                           if not op or op == "==" then
+                               op = ""
+                           end
+                           
+                           cgilua.put("<tr>")
+                           local testclass = "passed"
+                           if not test.passed then
+                               testclass = "failed"
+                           end
+                           cgilua.put([[<td class="]]..testclass..[[">]]..linenumber.."</td>")
+                           cgilua.put([[<td class="]]..testclass..[[">]]..test.exp1.."</td>")
+                           cgilua.put([[<td class="]]..testclass..[[">]]..op..val2.."</td>")
+                           cgilua.put([[<td class="]]..testclass..[[">]]..tostring(test.val1).."</td>")
+                           if not test.passed then
+                               cgilua.put([[<td class="]]..testclass..[[">]]..(test.msg or "").."</td>")
+                           else
+                               cgilua.put([[<td class="]]..testclass..[[">]].."</td>")
+                           end
+                           cgilua.put("</tr>")
+                       end
+                       cgilua.put("</table>")
+                   end
+               end
+           end
+       end
+   end
+end
+
+-- Reports a module Shake summary as a HTML table
+function ModuleSummary(run)
+   return function()
+   local results = run.results
+   for cs, suite in ipairs(results.suites) do
+       cgilua.put("<tr>\n")
+       cgilua.put("<th><strong>"..suite.title.."</strong></th>\n")
+       if suite.error then
+           cgilua.put([[<td>]]..suite.passed + suite.failed.."</td>\n")
+           cgilua.put([[<td]]..(((suite.failed > 0) and [[ class="failed">]]..suite.failed) or ">").."</td>\n")
+           cgilua.put([[<td class="error">1</td>]].."\n")
+       elseif suite.failed > 0 then
+           cgilua.put([[<td>]]..suite.passed + suite.failed.."</td>\n")
+           cgilua.put([[<td class="failed">]]..suite.failed.."</td>\n")
+           cgilua.put([[<td class="noerror"></td>]].."\n")
+       else
+           cgilua.put([[<td class="passed">]]..suite.passed + suite.failed.."</td>\n")
+           cgilua.put([[<td class="passed"></td>]].."\n")
+           cgilua.put([[<td class="noerror"></td>]].."\n")
+       end
+       cgilua.put("</tr>\n")
+   end
+   end
+end
+
+-- Reports the full Shake summary as HTML
+function ShakeSummary(run)
+	return function()
+		local results = run.results
+		if results.errors > 0 or results.failed > 0 then
+		    cgilua.put("<p>Shake output:</p>")
+		    cgilua.put([[<pre class="example">]])
+		    cgilua.put(run:summary())
+		    cgilua.put("</pre>")
+		end
+	end
+end
--- src/apps/shake/init.lua
+++ src/apps/shake/init.lua
@@ -1,199 +1,66 @@
--- This is an example of a Shake runner that uses CGILua to offer a drill down of the test results
+-- Shake runner that uses CGILua to offer a drill down of the test results
+--
+-- This file acts as the general controller for the application using URLs like:
+-- /shake/all - Shakes all the modules in SHAKE_TESTS
+-- /shake/module - Shake a specific module in SHAKE_TESTS
+-- /shake/... - Processes the resource as a path to a file in the Shake app home directory
+
 require"shake"
 
+-- get the optional configuration
+cgilua.doif("shake_conf.lua")
 
-local function init()
-    local SHAKE_TESTS = KEPLER_CONF.."/tests"
-    -- get the optional configuration
-    cgilua.doif("shake_conf.lua")
-    local SHAKE_VIRTUAL_DIR = SHAKE_VIRTUAL_DIR or "/"
+-- load output helper functions
+cgilua.doif("helper.lua")
 
-    local suite, _ = cgilua.splitfirst(cgilua.script_vpath)
+local SHAKE_TESTS = SHAKE_TESTS or KEPLER_CONF.."/tests"
 
-    cgilua.script_vpath = nil
-    
-    -- Finds all available suites
-    local suites = {}
-    for dir in lfs.dir(SHAKE_TESTS) do
-        local attr = lfs.attributes (SHAKE_TESTS.."/"..dir)
-        if attr.mode == "directory" and dir ~= "." and dir ~= ".." then
-            table.insert(suites, dir)
-        end
-    end
-    table.sort(suites)    
+local path_info = cgilua.script_vpath
+local cmd, rest = cgilua.splitfirst(path_info)
 
-    local run = shake.runner()
+cmd = cmd or "all"
 
+-- Finds all available modules
+local modules = {}
+for dir in lfs.dir(SHAKE_TESTS) do
+   local attr = lfs.attributes (SHAKE_TESTS.."/"..dir)
+   if attr.mode == "directory" and dir ~= "." and dir ~= ".." then
+       table.insert(modules, dir)
+   end
+end
+table.sort(modules)
 
-    local function ListSuites()
-        if not next(suites) then
-            cgilua.put("<li><strong>(No suites)</strong></li>")
-        else
-            for _,suite in ipairs(suites) do
-                cgilua.put([[<li><strong><a href="]]..cgilua.mkurlpath("shake/"..suite)..[[">]]..suite..[[</a></strong></li>]])
-            end
-        end
-    end
-    
-    local function ReportSuites(run)
-        return function()
-        local results = run.results
-        for cs, suite in ipairs(results.suites) do
-            cgilua.put("<tr>\n")
-            cgilua.put("<th><strong>"..[[<a href="]]..cgilua.mkurlpath("shake/"..suite.title)..[[">]]..suite.title..[[</a></strong></th>]].."\n".."</strong></th>\n")
-            if suite.error then
-                cgilua.put([[<td>]]..suite.passed + suite.failed.."</td>\n")
-                cgilua.put([[<td]]..(((suite.failed > 0) and [[ class="failed">]]..suite.failed) or ">").."</td>\n")
-                cgilua.put([[<td class="error">1</td>]].."\n")
-            elseif suite.failed > 0 then
-                cgilua.put([[<td>]]..suite.passed + suite.failed.."</td>\n")
-                cgilua.put([[<td class="failed">]]..suite.failed.."</td>\n")
-                cgilua.put([[<td class="noerror"></td>]].."\n")
-            else
-                cgilua.put([[<td class="passed">]]..suite.passed + suite.failed.."</td>\n")
-                cgilua.put([[<td class="passed"></td>]].."\n")
-                cgilua.put([[<td class="noerror"></td>]].."\n")
-            end
-            cgilua.put("</tr>\n")
-        end
-        cgilua.put("<tfoot>")
-        cgilua.put("<tr>\n")
-        cgilua.put("<th><strong>Totals</strong></th>\n")
-        cgilua.put("<td>"..results.failed + results.passed.."</td>\n")
-        cgilua.put([[<td class="]]..((results.failed > 0) and ([[failed">]]..results.failed) or [[passed">]]).."</td>\n")
-        cgilua.put([[<td class="]]..((results.errors > 0) and ([[error">]]..results.errors) or [[noerror">]]).."</td>\n")
-        cgilua.put("</tr>\n")
-        cgilua.put("</tfoot>")
-        end
-    end
-    
-    local function ReportSuite(run)
-        return function()
-            local results = run.results
-            for _, suite in ipairs(results.suites) do
-                if suite.error == -1 then
-                    cgilua.put("<p><strong>ERROR</strong>:</p>")
-                    cgilua.put("<p>"..suite.error.."</p>")
-                else
-                    for _, context in ipairs(suite.contexts) do
-                        if next(context.tests) then
-                            if context.output[1] ~= "" or context.comments then
-                                cgilua.put([[<div class="shakecontext">]].."\n")
-                                for _, output in ipairs(context.output) do
-                                    if output and output ~= "" then
-                                        cgilua.put([[<div class="shakeoutput">]]..output.."</div>\n")
-                                    end
-                                end
-                                if context.comments and context.comments ~= "" then
-                                    cgilua.put([[<div class="shakecomment">]]..context.comments.."</div>\n")
-                                end
-                                cgilua.put("</div>")
-                            end
-                            cgilua.put([[
-                            <table class="shake">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Line #</th>
-                                        <th scope="col"></th>
-                                        <th scope="col"><strong>Expected</strong></th>
-                                        <th scope="col"><strong>Actual</strong></th>
-                                        <th scope="col"><strong>Message</strong></th>
-                                    </tr>
-                                </thead>]])
-                            for _, test in ipairs (context.tests) do
-                                local linenumber = test.linenumber or "???"
-                                local op = test.op
-                                local val2 = tostring(test.val2)
-                                
-                                if not op then
-                                    val2 = "<em><strong>True value</strong></em>"
-                                end
-                                
-                                if not op or op == "==" then
-                                    op = ""
-                                end
-                                
-                                cgilua.put("<tr>")
-                                local testclass = "passed"
-                                if not test.passed then
-                                    testclass = "failed"
-                                end
-                                cgilua.put([[<td class="]]..testclass..[[">]]..linenumber.."</td>")
-                                cgilua.put([[<td class="]]..testclass..[[">]]..test.exp1.."</td>")
-                                cgilua.put([[<td class="]]..testclass..[[">]]..op..val2.."</td>")
-                                cgilua.put([[<td class="]]..testclass..[[">]]..tostring(test.val1).."</td>")
-                                if not test.passed then
-                                    cgilua.put([[<td class="]]..testclass..[[">]]..(test.msg or "").."</td>")
-                                else
-                                    cgilua.put([[<td class="]]..testclass..[[">]].."</td>")
-                                end
-                                cgilua.put("</tr>")
-                            end
-                            cgilua.put("</table>")
-                        end
-                    end
-                end
-            end
-        end
-    end
-    
-    local function SuiteSummary(run)
-        return function()
-        local results = run.results
-        for cs, suite in ipairs(results.suites) do
-            cgilua.put("<tr>\n")
-            cgilua.put("<th><strong>"..suite.title.."</strong></th>\n")
-            if suite.error then
-                cgilua.put([[<td>]]..suite.passed + suite.failed.."</td>\n")
-                cgilua.put([[<td]]..(((suite.failed > 0) and [[ class="failed">]]..suite.failed) or ">").."</td>\n")
-                cgilua.put([[<td class="error">1</td>]].."\n")
-            elseif suite.failed > 0 then
-                cgilua.put([[<td>]]..suite.passed + suite.failed.."</td>\n")
-                cgilua.put([[<td class="failed">]]..suite.failed.."</td>\n")
-                cgilua.put([[<td class="noerror"></td>]].."\n")
-            else
-                cgilua.put([[<td class="passed">]]..suite.passed + suite.failed.."</td>\n")
-                cgilua.put([[<td class="passed"></td>]].."\n")
-                cgilua.put([[<td class="noerror"></td>]].."\n")
-            end
-            cgilua.put("</tr>\n")
-        end
-        end
-    end
-    
-    local function ShakeSummary()
-        local results = run.results
-        if results.errors > 0 or results.failed > 0 then
-            cgilua.put("<p>Shake output:</p>")
-            cgilua.put([[<pre class="example">]])
-            cgilua.put(run:summary())
-            cgilua.put("</pre>")
-        end
-    end
+local run = shake.runner()
+local curr_dir = lfs.currentdir ()
 
-    local curr_dir = lfs.currentdir ()
-
-    if suite == nil or suite == "" or suite == "all" then
-        -- Runs all suites
-        for _, suite in ipairs(suites) do
-            lfs.chdir(SHAKE_TESTS.."/"..suite)
-            run:test("test.lua", suite)
-            lfs.chdir("..")
-        end
-        lfs.chdir(curr_dir)
-        cgilua.handlelp("shake.lp", {SHAKE_TESTS = SHAKE_TESTS, SHAKE_VIRTUAL_DIR = SHAKE_VIRTUAL_DIR,
-                                    ListSuites = ListSuites, ReportSuites = ReportSuites(run),
-                                    ShakeSummary = ShakeSummary, cgilua = cgilua})
-    else
-        lfs.chdir(SHAKE_TESTS.."/"..suite)
-        run:test("test.lua", suite)
-        lfs.chdir(curr_dir)
-        cgilua.handlelp("shake_suite.lp", {SHAKE_TESTS = SHAKE_TESTS, SHAKE_VIRTUAL_DIR = SHAKE_VIRTUAL_DIR,
-                                    suite = suite, ListSuites = ListSuites,
-                                    ReportSuite = ReportSuite(run), SuiteSummary = SuiteSummary(run, suite),
-                                    ShakeSummary = ShakeSummary, cgilua = cgilua})
-    end
-    return true
+if cmd == "all" then
+	-- Shakes all modules
+	for _, module_name in ipairs(modules) do
+	    lfs.chdir(SHAKE_TESTS.."/"..module_name)
+	    run:test("test.lua", module_name)
+	    lfs.chdir("..")
+	end
+	lfs.chdir(curr_dir)
+	local env = {
+		SHAKE_TESTS = SHAKE_TESTS, ListModules = ListModules(modules), ReportModules = ReportModules(run),
+		ShakeSummary = ShakeSummary(run), cgilua = cgilua
+	}
+	cgilua.handlelp("shake.lp", env)
+elseif cmd == "module" then
+	-- Shakes a single module
+	local module_name = cgilua.splitfirst(rest)
+	lfs.chdir(SHAKE_TESTS.."/"..module_name)
+	run:test("test.lua", module_name)
+	lfs.chdir(curr_dir)
+	local env = {
+		SHAKE_TESTS = SHAKE_TESTS, module_name = module_name, ListModules = ListModules(modules),
+		ReportModule = ReportModule(run, module_name), ModuleSummary = ModuleSummary(run, module_name),
+		ShakeSummary = ShakeSummary(run), cgilua = cgilua
+	}
+	cgilua.handlelp("shake_suite.lp", env)
+else
+	-- processes resource URLs
+	cgilua.handle(cgilua.script_pdir..path_info)
 end
 
-return init()
+return true
